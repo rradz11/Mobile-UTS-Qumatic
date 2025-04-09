2b3fe65db6409998f52f643120aae66b23f4a911
078_ivan budi saputra <149118034+pan8strong@users.noreply.github.com>
2025-04-09 15:01:51 +0700

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,4 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_screen.dart';
 
+// Abstraction
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(milliseconds: 500), () {  // Teks muncul perlahan 1.5 detik
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
+
+    Future.delayed(Duration(seconds: 3), () { // Animasi pindah 3 detik
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration: Duration(milliseconds: 800),
+          pageBuilder: (_, __, ___) => HomeScreen(),
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
