2dfc57fc12601ab21e469eee8a3ad543e13a57a0
078_ivan budi saputra <149118034+pan8strong@users.noreply.github.com>
2025-04-09 14:58:19 +0700

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,1 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'main.dart';
 
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFD8E1FF),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Selamat datang di Qumatic!',
+                style: GoogleFonts.konkhmerSleokchher(
+                  fontSize: 28,
+                  color: Color(0xFF002D8B),
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 40),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color(0xFF002D8B),
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(
+                  'Mulai Kuis',
+                  style: GoogleFonts.konkhmerSleokchher(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  Future.delayed(Duration(milliseconds: 420), () { // Animasi pindah
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: Duration(milliseconds: 800),
+                        pageBuilder: (_, __, ___) => QuizPage(),
+                        transitionsBuilder: (_, animation, __, child) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: child,
+                          );
+                        },
+                      ),
+                    );
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
