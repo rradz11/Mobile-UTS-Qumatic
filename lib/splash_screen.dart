e9e756c18f17d71df4c853f4ad675f1dbfc7416a
SavinkaKrizanantha <149308552+savinkaa@users.noreply.github.com>
2025-04-09 15:11:06 +0700

    Update splash_screen.dart

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -33,32 +33,3 @@ class _SplashScreenState extends State<SplashScreen> {
       );
     });
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xFF002D8B), Color(0xFF000C25)],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: AnimatedOpacity(
-            duration: Duration(milliseconds: 1500),
-            opacity: _opacity,
-            child: Text(
-              'Qumatic',
-              style: GoogleFonts.konkhmerSleokchher(
-                fontSize: 30,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
